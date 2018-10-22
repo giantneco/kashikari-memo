bba1f507d93a1b4c934844328f5898aa8359de2b
giantneco <giantneco@gmail.com>
2018-10-23 06:51:43 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main () => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'かしかりメモ',
+      home: List(),
+    );
+  }
+}
+
+class List extends StatefulWidget {
+  @override
+  _MyList createState() => _MyList();
+}
+
+class _MyList extends State<List> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("リスト画面"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: StreamBuilder<QuerySnapshot>(
+          stream: Firestore.instance.collection('kashikari-memo').snapshots(),
+          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (!snapshot.hasData) return const Text('Loading');
+            return ListView.builder(
+              itemCount: snapshot.data.documents.length,
+              padding: const EdgeInsets.only(top: 10.0),
+              itemBuilder: (context, index) =>
+                  _buildListItem(context, snapshot.data.documents[index]),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
+    return Card(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          ListTile(
+            leading: const Icon(Icons.android),
+            title: Text("【" + (document['borrowOrLend'] == "lend" ? "貸" : "借")
+                + "】" + document['stuff']),
+            subtitle: Text('期限 : '
+                + document['date'].toString().substring(0,10)
+                + "\n相手 : " + document['user']),
+          ),
+        ],
+      )
+    );
+  }
+}
